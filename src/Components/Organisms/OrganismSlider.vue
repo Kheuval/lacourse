39be8c262667be806b5cb563357411dbe84dbee2
Kheuval <hadrien.hartstein@gmail.com>
2022-12-04 18:41:51 +0100

    Add autoplay to slider

--- src/Components/Organisms/OrganismSlider.vue
+++ src/Components/Organisms/OrganismSlider.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="snap-x snap-mandatory flex overflow-x-auto rounded-3xl scrollbar-hide bg-black"
+    class="snap-x snap-mandatory flex overflow-x-auto rounded-3xl scrollbar-hide bg-black scroll-smooth"
+    ref="container"
   >
     <MoleculeSlide
       v-for="recipe in recipes"
@@ -15,12 +16,29 @@
 <script lang="ts" setup>
 import type { Recipe } from "@/Domain/Recipe/RecipeInterface";
 import { databaseRecipeRepository } from "@/Domain/Recipe/Repository/DatabaseRecipeRepository";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import MoleculeSlide from "../Molecules/MoleculeSlide.vue";
 
 const recipes = ref<Recipe[] | null>(null);
 
 databaseRecipeRepository.getSample().then((data) => (recipes.value = data));
+
+const container = ref<HTMLDivElement | null>(null);
+
+onMounted(() => {
+  const slideWidth = container.value!.clientWidth;
+
+  setInterval(() => {
+    if (
+      container.value!.scrollLeft ===
+      slideWidth * (recipes.value!.length - 1)
+    ) {
+      container.value!.scrollLeft = 0;
+    } else {
+      container.value!.scrollLeft += slideWidth;
+    }
+  }, 3000);
+});
 </script>
 
 <style lang="scss" scoped></style>
