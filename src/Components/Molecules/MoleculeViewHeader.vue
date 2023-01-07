b99c60f5a1083ab9177c841e71dfabe8ce87907e
Kheuval <hadrien.hartstein@gmail.com>
2023-01-07 18:03:08 +0100

    Add MoleculeViewHeader

---
+++ src/Components/Molecules/MoleculeViewHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex items-center justify-center">
+    <AtomLink :to="previousRoute" class="mr-auto">
+      <AtomIcon icon="fa-solid fa-chevron-left" class="text-4xl" />
+    </AtomLink>
+    <AtomTitle tag="h2" class="text-2xl text-center mr-auto p-6">
+      {{ title }}
+    </AtomTitle>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import AtomIcon from "../Atoms/AtomIcon.vue";
+import AtomLink from "../Atoms/AtomLink.vue";
+import AtomTitle from "../Atoms/AtomTitle.vue";
+
+const router = useRouter();
+
+defineProps<{
+  title: string;
+}>();
+
+const previousRoute = ref(router.options.history.state.back!.toString());
+</script>
